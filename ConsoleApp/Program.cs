78a091f4d5def86490c676869e74ef01058205b0
GursidhSandhu <gsandhu168@my.bcit.ca>
2024-01-17 22:49:08 -0800

    console connects, writes and reads to server. code needs cleaning up

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Text;
 
 namespace ConsoleApp
 {
@@ -20,7 +21,51 @@ namespace ConsoleApp
                     if (clientSocket.Connected)
                     {
                         Console.WriteLine("Connected to the server!");
-                        // Additional logic for data transmission and receiving JSON response will go here
+                        // Read the content of the file (tst.txt) from the same directory
+                        string filePath = "../../../tst.txt";
+                        string fileContent = File.ReadAllText(filePath);
+
+                        // Construct a POST request with form data and file content
+                        string boundary = "----WebKitFormBoundaryABC123";
+                        string formData = $"--{boundary}\r\n" +
+                                          "Content-Disposition: form-data; name=\"myText\"\r\n\r\n" +
+                                          "YourName\r\n" +
+                                          $"--{boundary}\r\n" +
+                                          $"Content-Disposition: form-data; name=\"filename\"; filename=\"{Path.GetFileName(filePath)}\"\r\n" +
+                                          "Content-Type: text/plain\r\n\r\n" +
+                                          $"{fileContent}\r\n" +
+                                          $"--{boundary}--\r\n";
+
+                        string userAgent = "MyCustomClientApp/1.0"; // Set our custom User-Agent here that the server will recognize
+
+                        string postRequest = $"POST /form HTTP/1.1\r\n" +
+                                             $"Host: {serverIp}\r\n" +
+                                             $"Content-Type: multipart/form-data; boundary={boundary}\r\n" +
+                                             $"Content-Length: {formData.Length}\r\n" +
+                                             $"User-Agent: {userAgent}\r\n\r\n" +
+                                             $"{formData}";
+
+                        byte[] requestBytes = Encoding.UTF8.GetBytes(postRequest);
+
+                        // Send the POST request to the server
+                        clientSocket.Send(requestBytes);
+
+                        // Read the response headers from the server
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = clientSocket.Receive(buffer);
+                        string responseHeaders = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        Console.WriteLine("Response Headers:\n" + responseHeaders);
+
+                        // Read and print the content of the response body (assuming JSON)
+                        StringBuilder responseBody = new StringBuilder();
+                        while (clientSocket.Available > 0)
+                        {
+                            bytesRead = clientSocket.Receive(buffer);
+                            responseBody.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+                        }
+
+                        Console.WriteLine("Response Body (JSON):\n" + responseBody.ToString());
                     }
                     else
                     {
